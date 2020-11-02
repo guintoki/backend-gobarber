0a17f94bcc3bf6ca4fdb1724d74c68074241f8c0
guintoki <guilherme-vilas-boas@hotmail.com>
2020-11-02 00:53:10 -0300

    change imports and add tsconfig - paths

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -3,10 +3,10 @@ import path from 'path';
 import fs from 'fs';
 import { getRepository } from 'typeorm';
 
-import AppError from '../../../errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-import uploadConfig from '../../../config/upload';
-import User from '../entities/User';
+import uploadConfig from '@config/upload';
+import User from '../infra/typeorm/entities/User';
 
 interface Request {
   user_id: string;
