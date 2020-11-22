0924e3de1c479e367843a0ab20e7ce4a595572c6
guintoki <guilherme-vilas-boas@hotmail.com>
2020-11-21 23:53:10 -0300

    add condictions to create appointments hours

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 import 'reflect-metadata';
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
@@ -28,10 +28,22 @@ class CreateAppointmentService {
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("you can't crete a appointment in a past date");
+    }
+
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
+    if (user_id === provider_id) {
+      throw new AppError("you can't crete a appointment with yourself");
+    }
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'you can only create appointments betwenn 8am and 5pm',
+      );
+    }
     if (findAppointmentInSameDate) {
       throw new AppError('This appointment is already reserved');
     }
