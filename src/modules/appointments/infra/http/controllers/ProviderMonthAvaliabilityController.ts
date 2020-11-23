dadddb321d860f34a1116ab6d367e67c770aff6c
guintoki <guilherme-vilas-boas@hotmail.com>
2020-11-22 23:19:09 -0300

    add provider month avaliability controller

---
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvaliabilityController.ts
@@ -0,0 +1,26 @@
+/* eslint-disable camelcase */
+import { Request, Response } from 'express';
+
+import { container } from 'tsyringe';
+
+import ListProviderMonthAvaliabilityService from '@modules/appointments/services/ListProviderMonthAvaliabilityService';
+
+export default class ProviderMonthAvaliabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+
+    const { month, year } = request.body;
+
+    const listProviderMonthAvaliabilit = container.resolve(
+      ListProviderMonthAvaliabilityService,
+    );
+
+    const avialibility = await listProviderMonthAvaliabilit.execute({
+      provider_id,
+      month,
+      year,
+    });
+
+    return response.json(avialibility);
+  }
+}
