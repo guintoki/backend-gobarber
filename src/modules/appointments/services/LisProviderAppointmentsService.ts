6d9a884365941327bb87ba576c541f53f074e8db
guintoki <guilherme-vilas-boas@hotmail.com>
2020-11-23 22:30:38 -0300

    update tests

---
+++ src/modules/appointments/services/LisProviderAppointmentsService.ts
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+import { inject, injectable } from 'tsyringe';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
+}
+
+@injectable()
+class LisProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        month,
+        year,
+        day,
+      },
+    );
+
+    return appointments;
+  }
+}
+
+export default LisProviderAppointmentsService;
