2b5951e0f1fbfaaae7182ee84a8c13816f08457c
guintoki <guilherme-vilas-boas@hotmail.com>
2020-11-24 22:33:08 -0300

    add segments to profile routes

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-shadow */
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+
 import ProfileController from '../controllers/ProfileController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
@@ -10,6 +12,18 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.put('/', profileController.update);
-profileRouter.get('/', profileController.show);
+profileRouter.get(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+      old_password: Joi.string(),
+    },
+  }),
+  profileController.show,
+);
 
 export default profileRouter;
