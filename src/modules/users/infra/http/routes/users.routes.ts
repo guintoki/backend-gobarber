f34053aea7405bee8f17c7481d90aa54c8f7c074
guintoki <guilherme-vilas-boas@hotmail.com>
2020-11-24 22:33:24 -0300

    add segments to user routes

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-shadow */
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 import UsersController from '../controllers/UsersController';
@@ -12,7 +14,17 @@ const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
