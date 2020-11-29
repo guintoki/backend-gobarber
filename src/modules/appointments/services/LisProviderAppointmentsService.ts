96a25e83217664c20bf6f7688823b7e32ed1d53b
guintoki <guilherme-vilas-boas@hotmail.com>
2020-11-29 17:05:39 -0300

    remove parsedDate

--- src/modules/appointments/services/LisProviderAppointmentsService.ts
+++ src/modules/appointments/services/LisProviderAppointmentsService.ts
@@ -28,16 +28,24 @@ class LisProviderAppointmentsService {
     year,
     day,
   }: IRequest): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('');
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
-      {
-        provider_id,
-        month,
-        year,
-        day,
-      },
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      'cleiton',
     );
+
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          month,
+          year,
+          day,
+        },
+      );
+
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
     return appointments;
   }
