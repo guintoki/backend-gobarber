50f5e1dd0e4b445511dac8d1a29c7035e89b5fd9
guintoki <guilherme-vilas-boas@hotmail.com>
2020-12-10 23:12:44 -0300

    fix userAvatarController

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from 'express';
-
 import { container } from 'tsyringe';
-import { classToClass } from 'class-transformer';
-
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import { classToClass } from 'class-transformer';
 
 export default class UserAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
@@ -13,7 +11,6 @@ export default class UserAvatarController {
       user_id: request.user.id,
       avatarFilename: request.file.filename,
     });
-
     return response.json(classToClass(user));
   }
 }
